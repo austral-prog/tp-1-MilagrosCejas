0dfcecbdd586c825fb214279c29ac8390e9b0356
MilagrosCejas <mcejas@mail.austral.edu.ar>
2025-03-21 11:49:25 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+    sum = a + b
+    difference = a - b
+    product = a * b
+    average = (a + b) / 2
+    integer_division = a // b
+    module_division = a % b
+    division = a / b
+
+    print(sum)
+    print(difference)
+    print(product)
+    print(average)
+    print(integer_division)
+    print(module_division)
+    print(division)
+math()
